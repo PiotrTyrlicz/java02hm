38676c905a67cb4fb72fb7043c2d8a952bc09be0
Piotr Tyrlicz <tyrlicz@wp.pl>
2017-09-18 12:29:21 +0200

    "Punkt 3 - Pytania.java"

---
+++ src/Pytania.java
@@ -0,0 +1,17 @@
+public class Pytania
+{
+    public static void main(String[] args) {
+        System.out.println("Klasa to: zbiór pól i metod które definiują obiekt" );
+        System.out.println("Obiekt to konretna rzecz mająca własne miejsce w pamięci komputera " +
+                "i jest stworzone na podstawie klasy");
+        System.out.println("Zmienna to pewna reprezentacja danych ");
+        System.out.println("Każda zmienna musi mieć określony typ");
+        System.out.println("W klasie mogą znajdować się pola, metody i konstruktory");
+        System.out.println("Obiekty są wywoływane przez operator new i służą do tworzenia nowych obiektów");
+        System.out.println("Jeśli w klasie zdefiniowano chociaż jeden konstruktor to kompilator nie wygeneruje dla nas konstruktora\n" +
+                "domyślnego");
+
+        System.out.println("car1 nazywamy referencja");
+        System.out.println("car1 i car2 to dwie referencje wskazujace na ten sam obiekt");
+    }
+}
